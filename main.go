f45a1f03d8c971fad1ffe04ae3bb83e7d4226cdd
ryan <ryan_lai@mail.chungyo.net>
2022-10-20 11:35:38 +0800

    test

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", helloWorld)
+	r.Run(":80")
+}
+
+func helloWorld(c *gin.Context) {
+	fmt.Println("Hello World!")
+}
+
+func unuseFunc() {
+	fmt.Println("This is an unuse function.")
+}
